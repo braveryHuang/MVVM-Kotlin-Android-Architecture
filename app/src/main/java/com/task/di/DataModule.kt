278209a6e453a1a4760327acbeb70005b9d19aad
huangyong <braveryhuang@126.com>
2025-10-11 11:55:00 +0800

    添加注释

--- app/src/main/java/com/task/di/DataModule.kt
+++ app/src/main/java/com/task/di/DataModule.kt
@@ -13,6 +13,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
+    /**
+     * 将 DataRepository 类的实例绑定为 DataRepositorySource 接口的实现。
+     *
+     * 1. 抽象方法的参数：
+     * dataRepository: DataRepository 是方法的参数，表示 Dagger 需要提供一个 DataRepository 的实例
+     * Dagger 会自动寻找如何创建 DataRepository 实例（通常是通过构造函数注入）
+     *
+     * 2. 返回类型：
+     * DataRepositorySource 是一个接口，是方法的返回类型
+     * 这告诉 Dagger，当有地方需要 DataRepositorySource 类型的依赖时，应该提供一个 DataRepository 的实例
+     *
+     * 3. 抽象类和抽象方法：
+     * 模块是抽象的，方法也是抽象的，因为 @Binds 只需要声明绑定关系，不需要方法体
+     * Dagger 在编译时会生成实际的实现代码
+     */
     @Binds
     @Singleton
     abstract fun provideDataRepository(dataRepository: DataRepository): DataRepositorySource
